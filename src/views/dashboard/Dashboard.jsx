5e5f4923487fe1c49c86d7866c9e4469c229ac03
diamondto <enzoblackred@gmail.com>
2024-08-01 23:15:58 +0700

    [feat/boilerplate-development] install tailwinds

--- src/views/dashboard/Dashboard.jsx
+++ src/views/dashboard/Dashboard.jsx
@@ -3,7 +3,12 @@ import React, { useEffect, useState } from 'react';
 const Dashbooard = () => {
     return (
         <div>
-            dashboard
+                <h1 className="text-3xl font-bold underline text-red-400">
+                    Hello world!
+                </h1>
+            <span className="material-icons md:text-2xl lg:text-2xl text-lg cursor-pointer">
+                close
+            </span>
         </div>
     )
 }
